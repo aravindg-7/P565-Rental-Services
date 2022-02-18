85fd798449f5d09e2be77d8683b707da021bb1e4
aravindg-7 <aravindganta7@gmail.com>
2022-02-18 02:30:06 -0500

    new_proj_with_dependencies

--- app/src/test/java/com/rental/RentalApplicationTests.java
+++ app/src/test/java/com/rental/RentalApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.rental;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RentalApplicationTests {
 
 	@Test
 	void contextLoads() {
